3888f09bdb7e156c39aa663cb4fc8a35b898cec1
wbonsu <34726639+wbonsu@users.noreply.github.com>
2020-03-23 12:12:55 -0400

    data and supporting files

---
+++ march-madness/cmake-build-debug/CMakeFiles/March_Madness_Simulator.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/March_Madness_Simulator.dir/Game.cpp.obj"
+  "CMakeFiles/March_Madness_Simulator.dir/Simulator.cpp.obj"
+  "CMakeFiles/March_Madness_Simulator.dir/Team.cpp.obj"
+  "March_Madness_Simulator.exe"
+  "March_Madness_Simulator.exe.manifest"
+  "March_Madness_Simulator.pdb"
+  "libMarch_Madness_Simulator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/March_Madness_Simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
